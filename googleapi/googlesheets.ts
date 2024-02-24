2bc2a35a92cf1c57fc726e132cbbb587f132979a
Bryant Le <8994291-LeftAutomated@users.noreply.replit.com>
2024-02-24 19:09:40 +0000

    feat: update officers

--- googleapi/googlesheets.ts
+++ googleapi/googlesheets.ts
@@ -1,52 +1,68 @@
-const { google } = require('googleapis');
+import { google } from 'googleapis';
+import {
+    MemberData,
+    MembersData
+} from "../interfaces";
 
-export async function runGS() {
-    const auth = new google.auth.GoogleAuth({
+type ColumnData = any[][] | any[] | null | undefined;
+    
+
+const auth = new google.auth.GoogleAuth({
         keyFile: "./googleapi/sheetkeys.json",
         scopes: "https://www.googleapis.com/auth/spreadsheets",
     });
 
-    const client = await auth.getClient();
+export async function runGS() {
+        const client = await auth.getClient();
 
     const googleSheets = google.sheets({ version: "v4", auth: client });
 
-    const spreadsheetId = "1Aun3NhxSLYnAzBL3EOfnxb3kB_8xEvLyvg1ZrBzacOA";
+    const spreadsheetId = "1bPJsKQK2kSC5D9mn_LoAInBqg-wQxk6oZuXYHkRUEsA";
 
+    const sheetName = "Members";
+    const columns = ['A', 'B', 'C'];
 
     const getNameRows = await googleSheets.spreadsheets.values.get({
         auth,
         spreadsheetId,
-        range: "Points!A:A",
+        range: `${sheetName}!${columns[0]}:${columns[0]}`,
     });
 
     const getDiscordTagRows = await googleSheets.spreadsheets.values.get({
         auth,
         spreadsheetId,
-        range: "Points!B:B",
+        range: `${sheetName}!${columns[1]}:${columns[1]}`,
     });
 
     const getPointRows = await googleSheets.spreadsheets.values.get({
         auth,
         spreadsheetId,
-        range: "Points!P:P",
+        range: `${sheetName}!${columns[2]}:${columns[2]}`,
     });
 
-    const nameData = getNameRows.data.values;
-    const tagData = getDiscordTagRows.data.values;
-    const pointData = getPointRows.data.values;
+    const nameData: ColumnData = getNameRows.data.values;
+    const tagData: ColumnData = getDiscordTagRows.data.values;
+    const pointData: ColumnData = getPointRows.data.values;
 
-    let memberData = [];
+    let membersData: MembersData = {};
 
-    for(let i = 1; i < nameData.length; i++){
-        let tempData = {
-            name: nameData[i],
-            tag: tagData[i],
-            point: pointData[i]
-        };
+    if (nameData != undefined && 
+        tagData != undefined && 
+        pointData != undefined && 
+        nameData != null && 
+        tagData != null &&
+        pointData != null) {
 
-        memberData.push(tempData);
+
+        for(let i = 1; i < nameData.length; i++){
+            let tempData: MemberData = {
+                name: nameData[i],
+                point: pointData[i]
+            };
+
+            membersData[tagData[i]] = tempData;
+        }
     }
-
-    return memberData;
+    return membersData;
 }
 
--- index.ts
+++ index.ts
@@ -9,6 +9,7 @@ import ProfileModule from "./modules/profile";
 import PointsModule from "./modules/points";
 import { run } from './instagram/instagram';
 import { startJob } from './timed_messages/newyear';
+import { testing } from "googleapis/build/src/apis/testing";
 
 // Configuration
 require('dotenv').config();
@@ -45,22 +46,43 @@ client.on('ready', () => {
 });
 
 // Message handling
+const sendWelcomeMessage = (message: any) => {
+  let rulesChannelId = "897566470387671092";
+  let ran = Math.floor((Math.random() * 10) + 1);
+    if (ran === 1)
+      message.channel.send(`I lost my life savings from dogecoin now go read <#${rulesChannelId}>`);
+    else if (ran === 2)
+      message.channel.send(`I bet you won't read <#${rulesChannelId}>`);
+    else if (ran === 3)
+      message.channel.send(`Read <#${rulesChannelId}> if you want rough brain`);
+};
+
+const deleteMessagesInWrongChannel = async(message: any, botCommandChannelId: string) => {
+  let botMessageId: string;
+  let userCommandMessageId = message.id;
+  message.channel.send(`Go to <#${botCommandChannelId}>. Both messages will be deleted in 15 seconds.`)
+      .then((msg: any) => {
+        botMessageId = msg.id;
+      });
+    
+    setTimeout(async() => {
+      if (message.channel.type !== "DM" && botMessageId !== null && userCommandMessageId !== null) {
+        await message.channel.messages.delete(userCommandMessageId);
+        await message.channel.messages.delete(botMessageId);
+      }
+    }, 15000);
+};
+
 client.on('messageCreate', async message => {
 
   // Skip if message from bot
   if (message.author.bot)
     return;
-
+  
   // Welcome channel is undefined when not found
   let welcomeChannel = message.guild?.channels.cache.find(c => c.name === 'welcome');
   if (welcomeChannel && message.channelId === welcomeChannel.id) {
-    let ran = Math.floor((Math.random() * 10) + 1);
-    if (ran === 1)
-      message.channel.send("I lost my life savings from dogecoin now go read <#897566470387671092>");
-    else if (ran === 2)
-      message.channel.send("I bet you won't read <#897566470387671092>");
-    else if (ran === 3)
-      message.channel.send("Read <#897566470387671092> if you want rough brain");
+    sendWelcomeMessage(message);
   }
 
   if (!message.content.startsWith(prefix))
@@ -75,17 +97,87 @@ client.on('messageCreate', async message => {
   let commandHandler = modules.get(command);
   // Abort when the command is undefined
   if (!commandHandler) return;
+  
+  // Restrict command access
+  let botCommandChannelId = '897596222255300658';
+  let testingChannelId = '897596353897721907';
+  if (message.channelId !== botCommandChannelId && message.channelId !== testingChannelId) {
+    await deleteMessagesInWrongChannel(message, botCommandChannelId);
+    return;
+  }
   // All commands use this interface
   if (commandHandler.command) commandHandler.command(message, args, client, modules);
 });
+
+const sendDeletedMessage = async (message: any) => {
+  let trackingChannelId = '967560432732766280';
+  await message.guild.channels.fetch(trackingChannelId)
+      .then((channel: any) => {
+        const embed = new Discord.MessageEmbed();
+        embed.setTitle(`Deleted Message`);
+        embed.setDescription(`<#${message?.channel?.id}>`);
+        embed.addField('Content', `${message?.content}`);
+        embed.addField('Author Information', `name: ${message?.author?.username}\ntag: ${message?.author?.tag}\nid: ${message?.author?.id}`);
+        embed.setColor('#2F4562');
+        embed.setTimestamp();
+
+        channel.send({
+          embeds: [embed]
+        });
+        
+        if (message.attachments.size > 0) {
+          channel.send(`__Attachments Deleted__`);
+          message.attachments.forEach((attachment: any) => {
+              const link = attachment.url;
+              channel.send({ 
+                files: [link] 
+              });
+          });
+        }
+      });
+};
+
+client.on("messageDelete", async (message) => {
+    if(!message.guild) 
+        return;
+
+    await sendDeletedMessage(message);
+});
+
+const sendEditedMessage = async (oldMessage: any, newMessage: any) => {
+  let trackingChannelId = '967560432732766280';
+  await oldMessage.guild.channels.fetch(trackingChannelId)
+        .then((channel: any) => {
+            const embed = new Discord.MessageEmbed();
+            embed.setTitle(`Edited Message`);
+            embed.setDescription(`<#${oldMessage?.channel?.id}>`);
+            embed.addField('Old Content', `${oldMessage?.content}`);
+            embed.addField('New Content', `${newMessage?.content}`);
+            embed.addField('Author Information', `name: ${oldMessage?.author?.username}\ntag: ${oldMessage?.author?.tag}\nid: ${oldMessage?.author?.id}`);
+            embed.setColor('#2F4562');
+            embed.setTimestamp();
+            
+            channel.send({
+                embeds: [embed]
+            });
+        });
+}
+
+client.on("messageUpdate", async (oldMessage, newMessage) => {
+    if(!oldMessage.guild)
+        return;
+
+    await sendEditedMessage(oldMessage, newMessage);
+}); 
 
 // Host server
 keepAlive();
 
 // Login with token
-client.login(token).catch(() => {
+client.login(token)/* .catch(() => {
+  
 });
-
+ */
 function InitializeModule(module: CoCoModule) {
   modules.set(module.name, module);
   if (module.service) module.service(client);
@@ -96,3 +188,4 @@ run();
 
 // Sends New Year Message
 startJob(client);
+
